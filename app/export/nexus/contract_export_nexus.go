efbe62a4feb120d55d9606f28e2b1fb1e007ba3b
1Zaitsev <4yu@bk.ru>
2022-06-25 11:53:23 +0300

    use D-funcitons

--- app/export/nexus/contract_export_nexus.go
+++ app/export/nexus/contract_export_nexus.go
@@ -3,10 +3,12 @@ package nexus
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	terra "github.com/terra-money/core/app"
 	"github.com/terra-money/core/app/export/util"
+	wasmkeeper "github.com/terra-money/core/x/wasm/keeper"
 	wasmtypes "github.com/terra-money/core/x/wasm/types"
 )
 
@@ -26,48 +28,20 @@ func ExportNexus(app *terra.TerraApp) (util.SnapshotBalanceAggregateMap, error) 
 
 	keeper := app.WasmKeeper
 
-	// get all LP token holders
-	var nAtomHolderMapFromLp = make(util.BalanceMap)
-	if err := util.GetCW20AccountsAndBalances(ctx, keeper, LpToken, nAtomHolderMapFromLp); err != nil {
-		return nil, fmt.Errorf("failed to fetch lp token holders; %v", err)
+	//get all nAsset wallets with balances
+	var nAssetHolders = make(util.BalanceMap)
+	if err := getCW20Balances(ctx, keeper, AddressNATOM, nAssetHolders); err != nil {
+		return nil, fmt.Errorf("failed to fetch nasset token holders; %v", err)
 	}
 
-	var LPTokenSupply struct {
-		TotalSupply sdk.Int `json:"total_supply"`
-	}
-	if err := util.ContractQuery(ctx, qs, &wasmtypes.QueryContractStoreRequest{
-		ContractAddress: LpToken,
-		QueryMsg:        []byte("{\"token_info\":{}}"),
-	}, &LPTokenSupply); err != nil {
-		return nil, fmt.Errorf("failed to fetch lp token total supply: %v", err)
-	}
-
-	var totalNAtomInLP struct {
-		Balance sdk.Int `json:"balance"`
-	}
-	query_string := fmt.Sprintf("{\"balance\":{%s}}", PsiNATOMPair)
-	if err := util.ContractQuery(ctx, qs, &wasmtypes.QueryContractStoreRequest{
-		ContractAddress: AddressNATOM,
-		QueryMsg:        []byte(query_string),
-	}, &totalNAtomInLP); err != nil {
-		return nil, fmt.Errorf("failed to fetch lp total nAtom amout: %v", err)
-	}
-
-	// todo: figure out how much bAsset was provided by lp token amount
-	// lp token balance/total supply = x
-	// pair nAsset balance * x;
-	for _, v := range nAtomHolderMapFromLp {
-		v = sdk.NewDecFromInt(v).QuoInt(LPTokenSupply.TotalSupply).MulInt(totalNAtomInLP.Balance).RoundInt()
-	}
-
-	// get all nAtom holders
-	var nAtomHolderMap = make(util.BalanceMap)
-	if err := util.GetCW20AccountsAndBalances(ctx, keeper, AddressNATOM, nAtomHolderMap); err != nil {
-		return nil, fmt.Errorf("failed to fetch nAtom holders: %v", err)
+	// get all nAsset from astro LP
+	var nAssetFromLP = make(util.BalanceMap)
+	if err := getLPBalances(ctx, keeper, qs, LpToken, PsiNATOMPair, AddressNATOM, nAssetFromLP); err != nil {
+		return nil, fmt.Errorf("failed to fetch nasset token holders from LP; %v", err)
 	}
 
 	// merge holder maps + nLUNA holdings from LP
-	mergednAtomHolderMap := util.MergeMaps(nAtomHolderMap, nAtomHolderMapFromLp)
+	mergednAtomHolderMap := util.MergeMaps(nAssetHolders, nAssetFromLP)
 
 	// iterate over merged nLUNA holder map, apply nLUNA -> bLUNA ratio
 	var finalBalance = make(util.SnapshotBalanceAggregateMap)
@@ -83,4 +57,83 @@ func ExportNexus(app *terra.TerraApp) (util.SnapshotBalanceAggregateMap, error) 
 	}
 
 	return finalBalance, nil
+}
+
+func getLPBalances(ctx context.Context, k wasmkeeper.Keeper, q wasmtypes.QueryServer, lpTokenAddress string, pairAddress string, nAssetAddr string, balanceMap map[string]sdk.Int) error {
+	lpSupply, err := util.GetCW20TotalSupply(ctx, q, lpTokenAddress)
+	if err != nil {
+		return err
+	}
+
+	pairNAssetBalance, err := util.GetCW20Balance(ctx, q, nAssetAddr, pairAddress)
+	if err != nil {
+		return err
+	}
+
+	prefix := util.GeneratePrefix("balance")
+	lpTokenAddr, err := sdk.AccAddressFromBech32(lpTokenAddress)
+	if err != nil {
+		return err
+	}
+	k.IterateContractStateWithPrefix(sdk.UnwrapSDKContext(ctx), lpTokenAddr, prefix, func(key, value []byte) bool {
+		if contract, _ := isContract(ctx, k, string(key)); contract {
+			return false
+		}
+		lpBalance, ok := sdk.NewIntFromString(string(value[1 : len(value)-1]))
+		if ok {
+			if lpBalance.IsZero() {
+				return false
+			}
+			balance := lpBalance.Mul(pairNAssetBalance).Quo(lpSupply)
+			if strings.Contains(string(key), "terra") {
+				balanceMap[string(key)] = balance
+			} else {
+				addr := sdk.AccAddress(key)
+				balanceMap[addr.String()] = balance
+			}
+		}
+		return false
+	})
+	return nil
+}
+
+func getCW20Balances(ctx context.Context, k wasmkeeper.Keeper, tokenAddress string, balanceMap map[string]sdk.Int) error {
+	prefix := util.GeneratePrefix("balance")
+	tokenAddr, err := sdk.AccAddressFromBech32(tokenAddress)
+	if err != nil {
+		return err
+	}
+	k.IterateContractStateWithPrefix(sdk.UnwrapSDKContext(ctx), tokenAddr, prefix, func(key, value []byte) bool {
+		if contract, _ := isContract(ctx, k, string(key)); contract {
+			return false
+		}
+		balance, ok := sdk.NewIntFromString(string(value[1 : len(value)-1]))
+		// fmt.Printf("%s %s\n", string(key), balance.String())
+		if ok {
+			if balance.IsZero() {
+				return false
+			}
+			if strings.Contains(string(key), "terra") {
+				balanceMap[string(key)] = balance
+			} else {
+				addr := sdk.AccAddress(key)
+				balanceMap[addr.String()] = balance
+			}
+		}
+		return false
+	})
+	return nil
+}
+
+func isContract(ctx context.Context, keeper wasmkeeper.Keeper, address string) (bool, error) {
+	contractAddr, err := sdk.AccAddressFromBech32(address)
+	if err != nil {
+		return true, err
+	}
+
+	if _, err := keeper.GetContractInfo(sdk.UnwrapSDKContext(ctx), contractAddr); err != nil {
+		return false, nil
+	}
+
+	return true, nil
 }
